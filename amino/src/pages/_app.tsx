13d755fa42d9779bcf5151ce3b1cfcfc6562138d
datschaub <karlssonlinus93@gmail.com>
2024-04-01 19:49:07 +0200

    init shadcnui

--- amino/src/pages/_app.tsx
+++ amino/src/pages/_app.tsx
@@ -1,17 +1,22 @@
 import { type AppType } from "next/app";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google"
+import { cn } from "~/lib/utils"
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
-const inter = Inter({
+const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-});
+})
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <main className={`font-sans ${inter.variable}`}>
+    <main className={cn(
+      "min-h-screen bg-background font-sans antialiased",
+      fontSans.variable
+    )}>
       <Component {...pageProps} />
     </main>
   );
